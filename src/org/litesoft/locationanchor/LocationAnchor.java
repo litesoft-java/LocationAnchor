4df5b34a6dbd8228bbd5b230266ca3a0bef5fdda
George Smith <litesoft@gmail.com>
2014-06-07 16:37:06 -0700

    Jusefuls Formatter Updated to New Code Format

--- src/org/litesoft/locationanchor/LocationAnchor.java
+++ src/org/litesoft/locationanchor/LocationAnchor.java
@@ -2,10 +2,8 @@
 package org.litesoft.locationanchor;
 
 @SuppressWarnings({"UnusedDeclaration"})
-public class LocationAnchor
-{
-    public String toString()
-    {
+public class LocationAnchor {
+    public String toString() {
         return getClass().getProtectionDomain().getCodeSource().getLocation().toString();
     }
 }
